c14d8b93bb78639a284bfd18fd5a6ab7cfb8ad6f
Kristin Lague <78080580+KristinLague@users.noreply.github.com>
2024-12-14 13:25:31 +0100

    Project Setup with Metal CPP

--- Apple-Metal-CPP/main.cpp
+++ Apple-Metal-CPP/main.cpp
@@ -5,6 +5,16 @@
 //  Created by Kristin Lague on 14/12/2024.
 //
 
+//Needs to be written, at one point, only once in a program somewhere
+#define NS_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#define MTK_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+
+#include <Metal/Metal.hpp>
+#include <AppKit/AppKit.hpp>
+#include <MetalKit/MetalKit.hpp>
+
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
