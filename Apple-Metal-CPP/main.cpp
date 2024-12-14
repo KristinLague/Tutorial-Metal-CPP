dccdd1a376279dcf2bde62f0c6ea5b4b37846f6e
Kristin Lague <78080580+KristinLague@users.noreply.github.com>
2024-12-14 13:09:38 +0100

    Initial Commit

---
+++ Apple-Metal-CPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Apple-Metal-CPP
+//
+//  Created by Kristin Lague on 14/12/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
